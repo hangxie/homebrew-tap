b79f4ed1a434a5c4d79df8e1482db9af0d349b77
Hang Xie <7977860+hangxie@users.noreply.github.com>
2021-05-23 19:16:18 -0700

    go-parquet-tools v1.0.3

---
+++ Formula/go-parquet-tools.rb
@@ -0,0 +1,27 @@
+class GoParquetTools < Formula
+  desc "Utility to deal with Parquet data"
+  homepage "https://github.com/hangxie/parquet-tools"
+  url "https://github.com/hangxie/parquet-tools/archive/v1.0.3.tar.gz"
+  sha256 "bb9ebff05e5eda226b11663e814943010a39558529ad66bc73d0ffdcdb371ebf"
+  license "BSD-3-Clause"
+
+  depends_on "go" => :build
+
+  conflicts_with "parquet-tools", because: "both install `parquet-tools` executables"
+
+  resource("test-parquet") do
+    url "https://github.com/hangxie/parquet-tools/raw/v1.0.3/cmd/testdata/good.parquet"
+    sha256 "d6ab36ac8bd23da136b7f8bd2a6c188db6421ea4e85870e247e57ddf554584ed"
+  end
+
+  def install
+    system "go", "build", "-ldflags", "-s -w -X main.version=v#{version} -X main.build=#{Time.now.iso8601}", *std_go_args, "-o", bin/"parquet-tools"
+  end
+
+  test do
+    resource("test-parquet").stage testpath
+
+    output = shell_output("#{bin}/parquet-tools schema #{testpath}/good.parquet")
+    assert_match "name=Parquet_go_root", output
+  end
+end
