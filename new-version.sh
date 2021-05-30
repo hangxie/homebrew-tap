2fd1bc2cf6f5766cdf51fc9c3be73f0997aaf83d
Hang Xie <7977860+hangxie@users.noreply.github.com>
2021-05-30 16:43:13 -0700

    update to v1.4.0, add utility to ease release

--- new-version.sh
+++ new-version.sh
@@ -1,16 +1,28 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [ -z "${VERSION}" ]; then
+    echo Usage: $(basename $0) version
+    exit 1
+fi
+
+SHA256=$(curl -sL https://github.com/hangxie/parquet-tools/archive/${VERSION}.tar.gz | shasum -a 256 | awk '{print $1}')
+
+cat > $(dirname $0)/Formula/go-parquet-tools.rb <<EOF
 class GoParquetTools < Formula
   desc "Utility to deal with Parquet data"
   homepage "https://github.com/hangxie/parquet-tools"
-  url "https://github.com/hangxie/parquet-tools/archive/v1.4.0.tar.gz"
-  sha256 "0a88b1ffdd8cada1b95a1f17a15075c86ff9cb0e4797d56e1c88a5c05a75a587"
+  url "https://github.com/hangxie/parquet-tools/archive/${VERSION}.tar.gz"
+  sha256 "${SHA256}"
   license "BSD-3-Clause"
 
   depends_on "go" => :build
 
-  conflicts_with "parquet-tools", because: "both install `parquet-tools` executables"
+  conflicts_with "parquet-tools", because: "both install \`parquet-tools\` executables"
 
   resource("test-parquet") do
-    url "https://github.com/hangxie/parquet-tools/raw/v1.4.0/cmd/testdata/good.parquet"
+    url "https://github.com/hangxie/parquet-tools/raw/${VERSION}/cmd/testdata/good.parquet"
     sha256 "d6ab36ac8bd23da136b7f8bd2a6c188db6421ea4e85870e247e57ddf554584ed"
   end
 
@@ -25,3 +37,4 @@ class GoParquetTools < Formula
     assert_match "name=Parquet_go_root", output
   end
 end
+EOF
