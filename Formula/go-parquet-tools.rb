b84629bb6d73096ebd049ede862d18a03e8fa9e7
Hang Xie <7977860+hangxie@users.noreply.github.com>
2022-07-07 06:13:01 -0700

    update to v1.14.1

--- Formula/go-parquet-tools.rb
+++ Formula/go-parquet-tools.rb
@@ -1,8 +1,8 @@
 class GoParquetTools < Formula
   desc "Utility to deal with Parquet data"
   homepage "https://github.com/hangxie/parquet-tools"
-  url "https://github.com/hangxie/parquet-tools/archive/v1.14.0.tar.gz"
-  sha256 "92fcfdda5857a2eb3f8ccbc2fbbe6591637d6e520dcd98430f9c23409929ade3"
+  url "https://github.com/hangxie/parquet-tools/archive/v1.14.1.tar.gz"
+  sha256 "89d74e669e307b70975854a48c744028c3e791630f3d820c34135014ddacdfdb"
   license "BSD-3-Clause"
 
   depends_on "go" => :build
@@ -10,7 +10,7 @@ class GoParquetTools < Formula
   conflicts_with "parquet-tools", because: "both install `parquet-tools` executables"
 
   resource("test-parquet") do
-    url "https://github.com/hangxie/parquet-tools/raw/v1.14.0/cmd/testdata/good.parquet"
+    url "https://github.com/hangxie/parquet-tools/raw/v1.14.1/cmd/testdata/good.parquet"
     sha256 "d6ab36ac8bd23da136b7f8bd2a6c188db6421ea4e85870e247e57ddf554584ed"
   end
 
