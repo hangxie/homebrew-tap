995ea51ac2371e5cd1abad00a2518d74e9462cc7
Hang Xie <7977860+hangxie@users.noreply.github.com>
2023-09-16 19:18:24 -0700

    update to v1.20.2

--- Formula/go-parquet-tools.rb
+++ Formula/go-parquet-tools.rb
@@ -1,19 +1,19 @@
 class GoParquetTools < Formula
   desc "Utility to deal with Parquet data"
   homepage "https://github.com/hangxie/parquet-tools"
-  url "https://github.com/hangxie/parquet-tools/archive/v1.20.1.tar.gz"
-  sha256 "963a7f96d336d0dc776647183414219d4408338cb25ce2e1843dede73ed4c7b7"
+  url "https://github.com/hangxie/parquet-tools/archive/v1.20.2.tar.gz"
+  sha256 "1dbefcf2160069ea518d598b768fce498482257d039bf7cf1b5363de229b3cc2"
   license "BSD-3-Clause"
 
   depends_on "go" => :build
 
   resource("test-parquet") do
-    url "https://github.com/hangxie/parquet-tools/raw/v1.20.1/testdata/good.parquet"
+    url "https://github.com/hangxie/parquet-tools/raw/v1.20.2/testdata/good.parquet"
     sha256 "daf5090fbc5523cf06df8896cf298dd5e53c058457e34766407cb6bff7522ba5"
   end
 
   def install
-    system "go", "build", "-ldflags", "-s -w -X github.com/hangxie/parquet-tools/cmd.version=v#{version} -X github.com/hangxie/parquet-tools/cmd.build=#{Time.now.iso8601} -X github.com/hangxie/parquet-tools/cmd.gitHash=7e9e7ea", *std_go_args, "-o", "#{bin}/parquet-tools"
+    system "go", "build", "-ldflags", "-s -w -X github.com/hangxie/parquet-tools/cmd.version=v#{version} -X github.com/hangxie/parquet-tools/cmd.build=#{Time.now.iso8601} -X github.com/hangxie/parquet-tools/cmd.gitHash=1ae88b2", *std_go_args, "-o", "#{bin}/parquet-tools"
   end
 
   test do
